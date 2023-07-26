f1b8ff756201b1bd1357eef9df7e4395116469ce
Katie Kermode <kermodekatie@gmail.com>
2023-07-26 14:16:47 +0100

    feat: add very basic training functionality

--- src/pages/training/index.js
+++ src/pages/training/index.js
@@ -1,5 +1,7 @@
 
 import { withPageAuthRequired, getSession} from "@auth0/nextjs-auth0";
+import { useState, useEffect} from "react";
+import { useRouter } from "next/router";
 import dbConnect from "@/lib/dbConnect";
 import Journey from "@/models/Journey";
 import MemoSystem from "@/models/MemoSystem";
@@ -7,11 +9,65 @@ import ImageSet from "@/models/ImageSet";
 // import SiteUser from "@/models/SiteUser";
 
 const TrainingCenter = ({user, journeys, imageSets, systems}) => {
+  const router = useRouter();
+  const contentType = 'application/json'
+  const imageSetID = router.query.imageSet;
+  const [imageSet, setImageSet] = useState({});
+  const [randImage, setRandImage] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setImageSet(getImageSet(imageSetID));
+    setIsLoading(false);
+  }, [imageSetID])
+
+  const getImageSet = async (id) => {
+    //get image set from DB
+    const res = await fetch(`/api/imageSets/${id}`, {
+      method: 'GET',
+      headers: {
+        Accept: contentType,
+        'Content-Type': contentType,
+      },
+      //body: JSON.stringify({...imageSet, images: imageArray}),
+    })
+    const data = await res.json();
+    console.log(data);
+    setImageSet(data.data);
+  }
+
+  const handleStartTraining = () => {
+    //get random image from set
+    const randIndex = Math.floor(Math.random() * imageSet.images.length);
+    setRandImage(imageSet.images[randIndex]);
+  }
+
+  const handleNextImage = () => {
+        //get random image from set
+        const randIndex = Math.floor(Math.random() * imageSet.images.length);
+        setRandImage(imageSet.images[randIndex]);
+  }
+
   return(
     <>
     <div className="z-10 justify-between font-mono text-lg max-w-5xl w-full ">
     <h1 className="py-2 font-mono text-4xl">Training Center</h1>
     <p className="font-mono">Hello {user.nickname} - there are {journeys.length} journeys, {imageSets.length} image sets and {systems.length} systems in the database.</p>
+    
+    {imageSet && !isLoading && 
+    <>
+    <div className="flex flex-col justify-center items-center">
+    <div className="mt-10 font-mono text-3xl">{imageSet.name}</div>
+    {!randImage.name && <button onClick={handleStartTraining} className="w-40 btn bg-white text-black font-bold mt-3 mx-0.5 py-1 px-4 rounded focus:outline-none focus:shadow-outline">Start</button>}
+    {randImage.name &&
+    <div className="flex flex-col justify-center items-center">{randImage.name}
+    <button onClick={handleNextImage} className="w-40 btn bg-white text-black font-bold mt-3 mx-0.5 py-1 px-4 rounded focus:outline-none focus:shadow-outline">Next</button>
+    </div>
+    }
+    </div>
+    </>
+    }
   </div>
 
 </>
