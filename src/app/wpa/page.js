485be516656c246d15d0e38e1fa73bacfb971944
Katie Kermode <kermodekatie@gmail.com>
2023-07-28 17:02:12 +0100

    feat: move main page to pages dir

--- src/app/wpa/page.js
+++ src/app/wpa/page.js
@@ -1,66 +1,66 @@
-// pages/dashboard.js
+// // pages/dashboard.js
 
-// import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+// // import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
-// const Dashboard = ({ user }) => {
-//   return <div>Hello {user.name}</div>;
-// };
+// // const Dashboard = ({ user }) => {
+// //   return <div>Hello {user.name}</div>;
+// // };
 
-// export const getServerSideProps = withPageAuthRequired();
+// // export const getServerSideProps = withPageAuthRequired();
 
-// export default Dashboard;
+// // export default Dashboard;
 
-// import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+// // import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
-// const Dashboard = withPageAuthRequired(({ user }) => {
-//   return <p>Welcome {user.name}</p>;
-// });
+// // const Dashboard = withPageAuthRequired(({ user }) => {
+// //   return <p>Welcome {user.name}</p>;
+// // });
 
-// export default Dashboard;
-
-
-
-
-//This is the same as pages/myPage/index.js but doesn't work as user is undefined!
-
-import { withPageAuthRequired, getSession} from "@auth0/nextjs-auth0";
-
-// const Dashboard = withPageAuthRequired(({ user }) => {
-//   return <p>Welcome {user.name}</p>;
-// });
-
-// export default Dashboard;
+// // export default Dashboard;
 
 
 
-const Dashboard = ({user}) => {
-  console.log("hi")
-  return(
-<p>Hello {user.name}</p>
-  )
-}
 
-export default Dashboard;
+// //This is the same as pages/myPage/index.js but doesn't work as user is undefined!
+
+// import { withPageAuthRequired, getSession} from "@auth0/nextjs-auth0";
+
+// // const Dashboard = withPageAuthRequired(({ user }) => {
+// //   return <p>Welcome {user.name}</p>;
+// // });
+
+// // export default Dashboard;
+
+
+
+// const Dashboard = ({user}) => {
+//   console.log("hi")
+//   return(
+// <p>Hello {user.name}</p>
+//   )
+// }
 
 // export default Dashboard;
-export const getServerSideProps = withPageAuthRequired({
-  getServerSideProps: async ({ req, res }) => {
-    const auth0User = getSession(req, res);
 
-    // Fetch the user from the db (by email)
-   // let user = await db.user.findUnique({ where: { email: auth0User?.user.email } });
 
-    // You might want to move the creation of the user somewhere else like afterCallback
-    // Checkout https://auth0.github.io/nextjs-auth0/modules/handlers_callback.html
-    // if (!user) {
-    //   user = db.user.create(auth0User?.user);
-    // } 
+// export const getServerSideProps = withPageAuthRequired({
+//   getServerSideProps: async ({ req, res }) => {
+//     const auth0User = getSession(req, res);
 
-    return {
-      props: {
-        // dbUser: user,
-        user: (await auth0User).user
-      },
-    };
-  },
-})
+//     // Fetch the user from the db (by email)
+//    // let user = await db.user.findUnique({ where: { email: auth0User?.user.email } });
+
+//     // You might want to move the creation of the user somewhere else like afterCallback
+//     // Checkout https://auth0.github.io/nextjs-auth0/modules/handlers_callback.html
+//     // if (!user) {
+//     //   user = db.user.create(auth0User?.user);
+//     // } 
+
+//     return {
+//       props: {
+//         // dbUser: user,
+//         user: (await auth0User).user
+//       },
+//     };
+//   },
+// })
---
+++ src/pages/index.js
@@ -0,0 +1,76 @@
+'use client';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
+
+export default function Index() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    return (      
+      <>
+       
+        
+           {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
+      <div className="z-10 justify-between font-mono text-lg max-w-5xl w-full ">
+      {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"> */}
+      <h1 className="font-mono text-6xl">System Trainer</h1>
+      <p>
+        Welcome {user.name}! 
+        {/* {user.nickname}  */}
+        {/* <a href="/api/auth/logout">Logout</a> */}
+        </p>
+       
+      
+       
+
+      
+
+      </div>
+     
+      </>
+    );
+  }
+
+  return <div>
+<h1>System Trainer app</h1>
+<a href="/api/auth/signup">Sign up</a>
+<a href="/api/auth/login">Log in</a>
+</div>;
+}
+
+
+/*
+//THIS WORKED ONCE I REMOVED THE COOKIE STUFF BUT DOESN'T DO QUITE WHAT I WANT
+import { UserProfile } from '@auth0/nextjs-auth0/client'
+
+const getUser = async (): Promise<UserProfile | null> => {
+  const sessionCookie = cookies().get('appSession')?.value
+  if (sessionCookie === undefined) return null
+  const res = await fetch(`${process.env.AUTH0_BASE_URL}/api/auth/me`, {
+    headers: {
+      cookie: `appSession=${sessionCookie}`,
+    },
+  })
+  return await (res.status === 200 ? res.json() : null)
+}
+
+const Home = async () => {
+  const user = await getUser();
+
+  return (
+    <p>
+      {JSON.stringify(user)}
+      <br/>
+      <a href="/api/auth/login">LOG IN</a>
+    </p>
+  )
+};
+
+export default Home
+
+*/
